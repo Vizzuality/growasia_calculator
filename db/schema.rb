53d40694dba3bff7f99c5c861532fe1b6fc179e9
Simao Belchior <simaobelchior@gmail.com>
2016-08-16 17:55:43 +0100

    Change from sqlite3 to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160816143642) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "geo_locations", force: :cascade do |t|
     t.string   "country"
     t.string   "state"
