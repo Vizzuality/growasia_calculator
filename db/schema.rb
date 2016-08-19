a85d1b6d8b881ffbf6428fa7a8b1385755ba6f05
Simao Belchior <simaobelchior@gmail.com>
2016-08-19 17:18:12 +0100

    Sets default route, ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816143642) do
+ActiveRecord::Schema.define(version: 20160819160528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "analyses", force: :cascade do |t|
+    t.integer  "geo_location_id"
+    t.decimal  "area"
+    t.decimal  "yield"
+    t.boolean  "is_shaded"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "geo_locations", force: :cascade do |t|
     t.string   "country"
