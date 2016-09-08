a653c19f02e00f58be0e85227301196e4669de15
Simao Belchior <simaobelchior@gmail.com>
2016-09-08 14:10:05 +0100

    Adds new json layout for analyses serializer. for stacked charts

--- app/serializers/analysis_serializer.rb
+++ app/serializers/analysis_serializer.rb
@@ -4,36 +4,158 @@ class AnalysisSerializer < ActiveModel::Serializer
   belongs_to :geo_location
 
   def analysis
-    result = []
+    result = {}
+    total = 0.0
+
+    # for stacked bars
+    emissions_by_source = []
     if !object.rice?
-      result << {stable_soil_carbon_content: object.stable_soil_carbon_content}
+      # Soil management
+      sub_total = object.stable_soil_carbon_content
+      emissions_by_source << {
+        slug: "soil-mgmt",
+        name: "Soil management",
+        total: sub_total,
+        values: [
+          {
+            slug: "soil-carbon-content",
+            name: "Stable soil carbon content",
+            value: sub_total
+          }
+        ]
+      }
+      total += sub_total
+
+      # shaded
       if object.is_shaded?
-        result << {changes_in_carbon_content: object.changes_in_carbon_content}
+        sub_total = object.changes_in_carbon_content
+        emissions_by_source << {
+          slug: "agroforestry",
+          name: "Agroforestry removals",
+          total: sub_total,
+          values: [
+            {
+              slug: "changes-carbon-content",
+              name: "Changes in carbon content",
+              value: sub_total
+            }
+          ]
+        }
+        total += sub_total
       end
-      result << {emissions_from_crop_residue_decomposition: object.emissions_from_crop_residue_decomposition}
-      result << {emissions_from_crop_residue: object.emissions_from_crop_residue_or_rice_straw_burning}
-    else
-      result << {emissions_from_rie_straw_residue: object.emissions_from_crop_residue_or_rice_straw_burning}
-      result << {emissions_from_rice_cultivation: object.emissions_from_rice_cultivation}
     end
-    result << {emissions_from_fertilizers_application: object.emissions_from_fertilizers_application}
+
+    # Nutrient management
+    nutrient_management = {
+      slug: "nutrient-mgmt",
+      name: "Nutrient Management"
+    }
+    sub_total = 0.0
+    values = []
+    if object.rice?
+      val = object.emissions_from_rice_cultivation
+      values << {
+        slug: "rice-cultivation",
+        name: "Rice cultivation",
+        value: val
+      }
+      sub_total += val
+    else
+      val = object.emissions_from_crop_residue_decomposition
+      values << {
+        slug: "residue-decomposition",
+        name: "Crop residue decomposition",
+        value: val
+      }
+      sub_total += val
+    end
+    val = object.emissions_from_crop_residue_or_rice_straw_burning
+    values << {
+      slug: "residue-burning",
+      name: "#{object.rice? ? "Rice straw" : "Crop residue"} burning",
+      value: val
+    }
+    sub_total += val
+
+    val = object.emissions_from_fertilizers_application
+    values << {
+      slug: "fertilizers",
+      name: "Fertilizers Application",
+      value: val
+    }
+    sub_total += val
 
     if object.fertilizers.where(addition_type: "urea").any?
-      result << {emissions_from_urea_hydrolysis: object.emissions_from_urea_hydrolysis}
+      val = object.emissions_from_urea_hydrolysis
+      values << {
+        slug: "urea",
+        name: "Urea hydrolysis",
+        value: val
+      }
+      sub_total += val
     end
+    nutrient_management[:total] = sub_total
+    nutrient_management[:values] = values
 
+    emissions_by_source << nutrient_management
+
+    # Liming
+    values = []
+    sub_total = 0.0
     if object.lime_amount && object.lime_amount > 0.0
-      result << {emissions_from_lime_use: object.emissions_from_lime_use}
+      val = object.emissions_from_lime_use
+      values << {
+        slug: 'liming',
+        name: 'Lime use',
+        value: val
+      }
+      sub_total += val
     end
     if object.dolomite_amount && object.dolomite_amount > 0.0
-      result << {emissions_from_dolomite_use:  object.emissions_from_dolomite_use}
+      val = object.emissions_from_dolomite_use
+      values << {
+        slug: 'dolomite',
+        name: 'Dolomite use',
+        value: val
+      }
+      sub_total += val
     end
-
+    if sub_total > 0.0
+      emissions_by_source << {
+        slug: 'liming',
+        name: 'Liming',
+        total: sub_total,
+        values: values
+      }
+    end
     if object.agrochemical_amount && object.agrochemical_amount > 0.0
-      result << {emissions_from_agrochemical_use: object.emissions_from_agrochemical_use}
+      sub_total = object.emissions_from_agrochemical_use
+      emissions_by_source << {
+        slug: "agrochemical",
+        name: "Agrochemical use",
+        total: sub_total,
+        values: [
+          {
+            slug: "agrochemical",
+            name: "Agrochemical use",
+            value: sub_total
+          }
+        ]
+      }
     end
-    result << {emissions_from_fossil_fuel_use: object.emissions_from_fossil_fuel_use}
-
-    result
+    sub_total = object.emissions_from_fossil_fuel_use
+    emissions_by_source << {
+      slug: "fuel",
+      name: "Fossil fuel use",
+      total: sub_total,
+      values: [
+        {
+          slug: "fuel",
+          name: "Fossil fuel use",
+          value: sub_total
+        }
+      ]
+    }
+    {emissions_by_source: [emissions_by_source]}
   end
 end
