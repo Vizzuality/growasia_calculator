1b7eabb3c8bd577db1828d3ca1f047e8dd73b6be
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2016-09-09 19:39:24 +0200

    Sidebar amnedments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def controller?(*controller)
+    controller.include?(params[:controller])
+  end
+
+  def action?(*action)
+    action.include?(params[:action])
+  end
+    
   def select_options data_source
     options_for_select(data_source.map{|t| [t[:title], t[:slug]]})
   end
