b639fe1498228deb047b37ca63e8b8be751f7709
Simao Belchior <simaobelchior@gmail.com>
2016-09-09 12:46:24 +0100

    Adding form to the sidebar. And helper methods

--- app/models/addition.rb
+++ app/models/addition.rb
@@ -1,3 +1,23 @@
 class Addition < ApplicationRecord
   has_enumeration_for :category, with: Category, create_helpers: true
+
+  def fertilizer_name
+    fert = Analysis::FERTILIZER_TYPES.select{|t| t[:slug] == addition_type}.first
+    fert && fert[:title] or "n/a"
+  end
+
+  def manure_name
+    manure = Analysis::MANURE_TYPES.select{|t| t[:slug] == addition_type}.first
+    manure && manure[:title] or "n/a"
+  end
+
+  def fuel_name
+    fuel = Analysis::FUEL_TYPES.select{|t| t[:slug] == addition_type}.first
+    fuel && fuel[:title] or "n/a"
+  end
+
+  def fuel_unit_name
+    fuel = Analysis::FUEL_UNITS.select{|t| t[:slug] == addition_type}.first
+    fuel && fuel[:title] or "n/a"
+  end
 end
