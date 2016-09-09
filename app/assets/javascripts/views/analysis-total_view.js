ac8d39ab0d8fa7b96dbe31c89675b493bef4bb78
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2016-09-09 18:58:14 +0200

    Analysis: emissions units. Y Axis label. Two totals

--- app/assets/javascripts/views/analysis-total_view.js
+++ app/assets/javascripts/views/analysis-total_view.js
@@ -7,7 +7,6 @@
   App.View.AnalysisTotal = Backbone.View.extend({
 
     template: HandlebarsTemplates['analysis-total'],
-    templateCompare: HandlebarsTemplates['analysis-total-compare'],
 
     initialize: function(settings) {
       if (!this.el) {
@@ -29,26 +28,18 @@
 
     listeners: function() {
       this.model.on('change', this.render.bind(this));
-      this.modelCompare.on('change', this.update.bind(this));
+      this.modelCompare.on('change', this.render.bind(this));
     },
 
     render: function() {
       var analysis = this.model.get('analysis');
-      this.$el.html(this.template({
-        total: d3.format('.2s')(analysis.total)
-      }));
-      console.log('render');
-    },
-
-    update: function() {
-      var analysis = this.model.get('analysis');
       var analysisCompare = this.modelCompare.get('analysis');
-      var difference = analysis.total - analysisCompare.total;
-      this.$el.html(this.templateCompare({
-        total: d3.format('.2s')(difference)
+
+      this.$el.html(this.template({
+        total: (analysis.total) ? d3.format('.3s')(analysis.total) : null,
+        totalCompare: (!!analysisCompare) ? d3.format('.3s')(analysisCompare.total) : null,
       }));
-      console.log('update');
-    }
+    },
 
   });
 
