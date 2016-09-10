48b5e172c31f1051dd1d91de5c24259e78d976b2
Clara Linos <clara.linos@gmail.com>
2016-09-10 13:08:03 +0200

    map showing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,7 @@
 //= require d3
 //= require c3
 //= require chosen.js
+//= require leaflet.js
 
 //= require_self
 
--- app/assets/javascripts/views/map_view.js
+++ app/assets/javascripts/views/map_view.js
@@ -6,6 +6,22 @@
 
   App.View.Map = Backbone.View.extend({
 
+    defaults: {
+      cartoAccount: 'goal16',
+      tolerance: .5,
+      query: {
+        all: 'SELECT * FROM countries',
+        region: ''
+      },
+      css: {
+        all: '#countries{ polygon-fill: #c1de11; polygon-opacity: 0.4; line-color: #fafb00; line-width: 2; line-opacity: 1; }',
+        region: ''
+      },
+      zoom: 4,
+      lat: 7.92,
+      lng: 112.8
+    },
+
     initialize: function(settings) {
       if (!this.el) {
         return;
@@ -14,8 +30,7 @@
       var opts = settings && settings.options ? settings.options : {};
       this.options = _.extend({}, this.defaults, opts );
 
-      console.log(this.options)
-      debugger
+      this.createMap();
 
       this.render();
     },
@@ -23,9 +38,76 @@
     listeners: function() {},
 
     render: function() {
-      console.log('render');
+
     },
+
+
+    createMap: function() {
+      // var southWest = L.latLng(-80, 124),
+      //     northEast = L.latLng(80, -124),
+      //     bounds = L.latLngBounds(southWest, northEast);
+
+      var mapOptions = {
+        zoom: this.options.zoom,
+        center: [this.options.lat, this.options.lng],
+        scrollWheelZoom: false,
+        zoomControl: false,
+        tileLayer: {
+          continuousWorld: false,
+          noWrap: true,
+        }
+      };
+
+      this.map = new L.Map(this.el, mapOptions);
+
+      /* Needed for the initialization of the SVG layer */
+      this.bounds = this.map.getBounds();
+
+      this.initLayer().done(function() {
+        this.map.addLayer(this.layer)
+      }.bind(this))
+    },
+
+
+    /**
+     * Init the layer and return a deferred Object
+     * @return {Object} jQuery deferred object
+     */
+    initLayer: function() {
+      var deferred = $.Deferred();
+      var query = this.options.query.all;
+
+      var request = {
+        layers: [{
+          'user_name': this.options.cartoAccount,
+          'type': 'cartodb',
+          'options': {
+            'sql': query,
+            'cartocss': this.options.css.all,
+            'cartocss_version': '2.3.0'
+          }
+        }]
+      };
+
+      $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+        url: 'https://'+ this.options.cartoAccount +'.cartodb.com/api/v1/map/',
+        data: JSON.stringify(request),
+      }).done(_.bind(function(data) {
+        var tileUrl = 'https://'+ this.options.cartoAccount +'.cartodb.com/api/v1/map/'+ data.layergroupid + '/{z}/{x}/{y}.png32';
+        this.layer = L.tileLayer(tileUrl, { noWrap: true, https: true });
+        return deferred.resolve();
+      }, this));
+
+      return deferred;
+    },
+
 
   });
 
 })(this.App);
+
