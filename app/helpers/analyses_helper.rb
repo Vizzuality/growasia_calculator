6d4d700d23748ba19f68c8df45c61e70384897f5
Simao Belchior <simaobelchior@gmail.com>
2016-09-28 15:24:47 +0100

    Adds dynamic fields for fertilizers and manures

--- app/helpers/analyses_helper.rb
+++ app/helpers/analyses_helper.rb
@@ -45,5 +45,18 @@ module AnalysesHelper
     end
   end
 
-
+  def link_to_add_fields(name, f, association, upper_classes=nil, bottom_classes=nil)
+    new_object = f.object.send(association).klass.new
+    id = new_object.object_id
+    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+      render(association.to_s.singularize + "_fields", f: builder)
+    end
+    if upper_classes
+      fields = fields.gsub("my-upper-class-replacement", upper_classes)
+    end
+    if bottom_classes
+      fields = fields.gsub("my-bottom-class-replacement", bottom_classes)
+    end
+    link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+  end
 end
