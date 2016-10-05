97179a3a7cd54e2e84912d4bfe5c1a367e75f9ec
Simao Belchior <simaobelchior@gmail.com>
2016-10-05 11:25:59 +0200

    Sets size

--- app/helpers/analyses_helper.rb
+++ app/helpers/analyses_helper.rb
@@ -61,6 +61,6 @@ module AnalysesHelper
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
       render(association.to_s.singularize + "_sidebar_fields", f: builder)
     end
-    link_to(image_tag("add.svg"), "#", class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+    link_to(image_tag("add.svg", size: "18x18"), "#", class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
   end
 end
